976fd0daa90b0b1832609372b60092af34cf2507
Chris <Chris.Kehx@Gmail.Com>
2017-04-14 19:52:28 -0300

    DebugTool/ErrorTool commenting

--- core/src/com/kehxstudios/atlas/tools/DebugTool.java
+++ core/src/com/kehxstudios/atlas/tools/DebugTool.java
@@ -3,17 +3,20 @@ package com.kehxstudios.atlas.tools;
 import com.badlogic.gdx.Gdx;
 
 /**
- * Created by ReidC on 2017-04-06.
+ * Basic debugging tool to log messages to console on any platform
  */
 
 public class DebugTool {
 
+    // Start of line text
     public static String title = "Debug";
 
+    // Print a single string to console
     public static void log(String message) {
         Gdx.app.error(title, message);
     }
 
+    // Prints two strings to console
     public static void log(String message1, String message2) {
         Gdx.app.error(title +" - "+ message1, message2);
     }
--- core/src/com/kehxstudios/atlas/tools/ErrorTool.java
+++ core/src/com/kehxstudios/atlas/tools/ErrorTool.java
@@ -3,17 +3,20 @@ package com.kehxstudios.atlas.tools;
 import com.badlogic.gdx.Gdx;
 
 /**
- * Created by ReidC on 2017-04-06.
+ * Basic error reporting tool to log messages to console on any platform
  */
 
 public class ErrorTool {
 
+    // Start of line text
     public static String title = "Error";
 
+    // Prints a single string to console
     public static void log(String message) {
         Gdx.app.error(title, message);
     }
 
+    // Prints two strings to console
     public static void log(String message1, String message2) {
         Gdx.app.error(title +" - "+ message1, message2);
     }
