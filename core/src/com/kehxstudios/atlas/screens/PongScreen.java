4470740dac38e59148e7e801e78d18ce794eb58a
Chris <Chris.Kehx@Gmail.Com>
2017-04-15 21:30:29 -0300

    More basic commenting

---
+++ core/src/com/kehxstudios/atlas/screens/PongScreen.java
@@ -0,0 +1,46 @@
+package com.kehxstudios.atlas.screens;
+
+import com.kehxstudios.atlas.other.ActionType;
+
+/**
+ * Created by ReidC on 2017-04-15.
+ */
+
+public class PongScreen extends AScreen {
+
+
+    @Override
+    public void resetScreen() {
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void trigger(ActionType type) {
+
+    }
+}
