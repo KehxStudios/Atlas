7a905641d9d42aa41d096d5ce896336c49db17e6
Christopher Reid <Christopher.Reid@KehxStudios.Com>
2017-05-01 11:57:54 -0300

    Update DebugTool.java
    Minor editing on GitHub site

--- core/src/com/kehxstudios/atlas/tools/DebugTool.java
+++ core/src/com/kehxstudios/atlas/tools/DebugTool.java
@@ -9,15 +9,29 @@ import com.badlogic.gdx.Gdx;
 public class DebugTool {
 
     // Start of line text
-    public static String title = "Debug";
+    private static String title = "Debug > ";
 
     // Print a single string to console
     public static void log(String message) {
-        Gdx.app.error(title, message);
+        print(message);
     }
-
+   
     // Prints two strings to console
     public static void log(String message1, String message2) {
-        Gdx.app.error(title +" - "+ message1, message2);
+        print(message1 + ", " + message2);
+    }
+    
+    // Prints an array of strings to console
+    public static void log(String[] messages) {
+        String finalMessage = "";
+        for (String message : messages) {
+            finalMessage += ", " + message;
+        }
+        print(finalMessage);
+    }
+    
+    // Prints the title with message
+    private void print(String message) {
+        Gdx.app.error(title, message);   
     }
 }
