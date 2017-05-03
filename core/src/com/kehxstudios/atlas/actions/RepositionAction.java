c0fcb38eff1650801018b965404b78e1cd55d60c
Christopher Reid <Christopher.Reid@KehxStudios.Com>
2017-05-03 09:20:13 -0300

    Update RepositionAction.java
    Minor edit on GitHub site to RepositionComponent.  Fused component with TeleportComponent using a boolean to either add or set the linked position.

--- core/src/com/kehxstudios/atlas/actions/RepositionAction.java
+++ core/src/com/kehxstudios/atlas/actions/RepositionAction.java
@@ -10,6 +10,7 @@ public class RepositionAction extends Action {
 
     private Vector2 position;
     private Vector2 actionValue;
+    private boolean teleportToActionValue;
 
     public Vector2 getPosition() { return position; }
     public void setPosition(Vector2 position) { this.position = position; }
@@ -17,9 +18,16 @@ public class RepositionAction extends Action {
     public Vector2 getActionValue() { return actionValue; }
     public void setActionValue(float x, float y) { actionValue.set(x,y); }
     public void addActionValue(float x, float y) { actionValue.add(x,y); }
+    
+    public boolean isTeleportToActionValue() { return teleportToActionValue; }
+    public void setTeleportToActionValue(boolean value) { teleportToActionValue = value; }
 
     @Override
     public void trigger() {
-        position.add(actionValue);
+        if (teleportToActionValue) {
+            position.set(actionValue);
+        } else {
+            position.add(actionValue);
+        }
     }
 }
