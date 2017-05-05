2a72fc2704b93aa35d1ae5cea1a72a5e8293621e
Christopher Reid <Christopher.Reid@KehxStudios.Com>
2017-05-05 08:46:48 -0300

    Minor edit to LoadingScreen on GitHib site
    Added super methods to each of the Screen interface methods

--- core/src/com/kehxstudios/atlas/screens/LoadingScreen.java
+++ core/src/com/kehxstudios/atlas/screens/LoadingScreen.java
@@ -32,26 +32,27 @@ public class LoadingScreen extends AScreen {
   
     @Override
     public void show() {
-
+        super.show();
     }
 
     @Override
     public void resize(int width, int height) {
-
+        super.resize(width, height);
     }
 
     @Override
     public void pause() {
-
+        super.pause();
     }
 
     @Override
     public void resume() {
-
+        super.resume();
     }
 
     @Override
     public void hide() {
-
+        super.hide();
     }
+
 }
