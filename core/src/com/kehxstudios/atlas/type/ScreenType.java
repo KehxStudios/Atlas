5015e0e2c8d3620d26ca30ee46c5453eb5138eb3
Christopher Reid <Christopher.Reid@KehxStudios.Com>
2017-05-09 13:07:39 -0300

    Edited ScreenTypes
    Added the variables for RecapJournalScreen

--- core/src/com/kehxstudios/atlas/type/ScreenType.java
+++ core/src/com/kehxstudios/atlas/type/ScreenType.java
@@ -17,9 +17,9 @@ public enum ScreenType {
     INTRO("Intro", "texturePacks/intro.atlas", 1080, 1920, IntroScreen.class),
     MAIN_MENU("Main Menu", "texturePacks/mainMenu.atlas", 1440, 2560, MainMenuScreen.class),
     FLAPPY_BAT("Flappy Bat", "texturePacks/flappyBat.atlas", 240, 400, FlappyBatScreen.class),
-    GENE_ROCKETS("Gene Rockets", "texturePacks/geneRockets.atlas", 512, 910, AScreen.class), // FUTURE SCREEN
-    PONG("Pong", "texturePacks/pong.atlas", 512, 910, PongScreen.class), // FUTURE SCREEN
-    RECAP_JOURNAL("Recap Journal", "texturePacks/recapJournal.atlas", 512, 910, AScreen.class), // FUTURE SCREEN
+    GENE_ROCKETS("Gene Rockets", "texturePacks/geneRockets.atlas", 512, 910, GeneRocketsScreen.class),
+    PONG("Pong", "texturePacks/pong.atlas", 512, 910, PongScreen.class),
+    RECAP_JOURNAL("Recap Journal", "texturePacks/recapJournal.atlas", 512, 910, RecapJournalScreen.class),
     LOADING("Loading", "texturePacks/loading.atlas", 512, 910, LoadingScreen.class),
     VOID("Void", "", 0, 0, AScreen.class);
 
