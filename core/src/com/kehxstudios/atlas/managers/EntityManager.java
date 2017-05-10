429a213f1099460265c506b25805164871f57585
Christopher Reid <Christopher.Reid@KehxStudios.Com>
2017-05-10 09:17:44 -0300

    Edited EntityManager on GitHub site
    - Organized and cleaned up some of the code
    - Added basic comments to all methods
    - Changed some Debug logs to Error logs
    - Added some Debug logs to ensure proper setup / screen change

--- core/src/com/kehxstudios/atlas/managers/EntityManager.java
+++ core/src/com/kehxstudios/atlas/managers/EntityManager.java
@@ -9,6 +9,7 @@ import com.kehxstudios.atlas.components.GraphicsComponent;
 import com.kehxstudios.atlas.components.PhysicsComponent;
 import com.kehxstudios.atlas.entities.Entity;
 import com.kehxstudios.atlas.tools.DebugTool;
+import com.kehxstudios.atlas.tools.ErrorTool;
 
 import java.util.ArrayList;
 
@@ -18,6 +19,7 @@ import java.util.ArrayList;
 
 public class EntityManager extends Manager {
 
+    // Holds instance of class, create new if not set
     private static EntityManager instance;
     public static EntityManager getInstance() {
         if (instance == null) {
@@ -26,62 +28,112 @@ public class EntityManager extends Manager {
         return instance;
     }
 
+    // ArrayList for all Entities created
     private ArrayList<Entity> entities;
 
     // ArrayLists for Entities/Components to be removed on tick
     private ArrayList<Entity> markedEntities;
     private ArrayList<Component> markedComponents;
 
+    // Constructor
     private EntityManager() {
         super();
         setup();
     }
     
+    // Initalizes the ArrayLists
+    @Override
+    protected void setup() {
+        entities = new ArrayList<Entity>();
+        markedEntities = new ArrayList<Entity>();
+        markedComponents = new ArrayList<Component>();
+        DebugTool.log("EntityManager_setup: Complete");
+    }
+    
+    // Called to removed any marked Components or Entities
+    @Override
+    public void tick(float delta) {
+        while (markedComponents.size() > 0) {
+            removeComponent(markedComponents.get(0).getEntity(), markedComponents.get(0));
+            markedComponents.remove(markedComponents.get(0));
+        }
+        while (markedEntities.size() > 0) {
+            removeEntity(entities.get(0));
+            markedEntities.remove(markedEntities.get(0));
+        }
+    }
+
+    // Called when loading a new screen
+    @Override
+    protected void loadScreenSettings() {
+        DebugTool.log("EntityManager_loadScreenSettings: Complete");
+    }
+
+    // Called when unloading the current screen
+    @Override
+    protected void removeScreenSettings() {
+        while (entities.size() > 0) {
+            removeEntity(entities.get(0));
+        }
+        DebugTool.log("EntityManager_removeScreenSettings: Complete");
+    }
+    
+    // Called to mark an Entity to be removed on @tick()
     public void markEntityForRemoval(Entity entity) {
         if (!markedEntities.contains(entity)) {
             markedEntities.add(entity);
+        } else {
+            ErrorTool.log("Failed to mark entity, already marked");  
         }
     }
 
+    // Called to mark a Component to be removed on @tick()
     public void markComponentForRemoval(Component component) {
         if (!markedComponents.contains(component)) {
             markedComponents.add(component);
+        } else {
+            ErrorTool.log("Failed to mark component, already marked");  
         }
     }
 
+    // Adds an Entity to current @entities ArrayList
     public void addEntity(Entity entity) {
         if (!entities.contains(entity)) {
             entities.add(entity);
         } else {
-            DebugTool.log("Failed to add entity to entities");
+            ErrorTool.log("Failed to add entity to entities");
         }
     }
 
+    // Removes an Entity from current @entities ArrayList including Components
     private void removeEntity(Entity entity) {
         if (entities.contains(entity)) {
             if (entity.getComponents().size() > 0) {
                 ArrayList<Component> components = entity.getComponents();
-                for (;components.size() > 0;) {
-                    removeComponent(entity, components.get(0));
+                for (Component component : components) {
+                    removeComponent(entity, component);
                 }
             }
             entities.remove(entity);
         } else {
-            DebugTool.log("Failed to find entity in entities");
+            ErrorTool.log("Failed to find entity in entities");
         }
     }
 
+    // Adds a Component to @entity
     public void addComponent(Entity entity, Component component) {
         if (entities.contains(entity)) {
             if (!entity.hasComponent(component)) {
                 entity.getComponents().add(component);
             } else {
+                ErrorTool.log("entity already contains component");
             }
         } else {
-            DebugTool.log("Failed to find entity in entities to add component_1");
+            ErrorTool.log("Failed to find entity in entities to add component_1");
         }
     }
 
+    // Removed a Component from @entity
     private void removeComponent(Entity entity, Component component) {
         if (entities.contains(entity)) {
             if (entity.hasComponent(component)) {
@@ -102,38 +154,10 @@ public class EntityManager extends Manager {
                 }
                 entity.getComponents().remove(component);
             } else {
-                DebugTool.log("Failed to find component in entity to remove component");
+                ErrorTool.log("Failed to find component in entity to remove");
             }
         } else {
-            //DebugTool.log("Failed to find entity in entities to remove component_2");
-        }
-    }
-
-    @Override
-    public void tick(float delta) {
-        while (markedComponents.size() != 0) {
-            removeComponent(markedComponents.get(0).getEntity(), markedComponents.get(0));
-        }
-        while (markedEntities.size() != 0) {
-            removeEntity(entities.get(0));
-        }
-    }
-    
-    private void setup() {
-        entities = new ArrayList<Entity>();
-        markedEntities = new ArrayList<Entity>();
-        markedComponents = new ArrayList<Component>();
-    }
-
-    @Override
-    protected void loadScreenSettings() {
-
-    }
-
-    @Override
-    protected void removeScreenSettings() {
-        while (entities.size() != 0) {
-            removeEntity(entities.get(0));
+            ErrorTool.log("Failed to find entity in entities to remove component_2");
         }
     }
 }
