35140676a3840c9968af0815aed9b5a958603154
Christopher Reid <Christopher.Reid@KehxStudios.Com>
2017-05-10 09:52:08 -0300

    Edited InputManager on GitHub site
    - Organized and cleaned up some of the code
    - Added basic comments to all methods
    - Changed some Debug logs to Error logs
    - Added some Debug logs to ensure proper setup / screen change

--- core/src/com/kehxstudios/atlas/managers/InputManager.java
+++ core/src/com/kehxstudios/atlas/managers/InputManager.java
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 
 public class InputManager extends Manager {
 
+    // Holds instance of class, create new if not set
     private static InputManager instance;
     public static InputManager getInstance() {
         if (instance == null) {
@@ -21,60 +22,72 @@ public class InputManager extends Manager {
         return instance;
     }
 
+    // ArrayList for all ClickableComponents created
     private ArrayList<ClickableComponent> clickableComponents;
 
+    // Constructor
     public InputManager() {
         super();
         setup();
     }
     
+    // Initalizes the @clickableComponents
+    @Override
     private void setup() {
         clickableComponents = new ArrayList<ClickableComponent>();
+        DebugTool.log("InputManager_setup: Complete");
     }
 
+    // Called to check if any clickableComponents were touched
+    @Override
     public void tick(float delta) {
         if (Gdx.input.justTouched() && clickableComponents.size() > 0) {
-            checkClickable();
-        }
-    }
-
-    private void checkClickable() {
-        float x = screen.getWidth() - Gdx.input.getX() / screen.getScaleWidth() + screen.getCamera().position.x - 
+            float x = screen.getWidth() - Gdx.input.getX() / screen.getScaleWidth() + screen.getCamera().position.x - 
                 screen.getCamera().viewportWidth/2;
-        float y = screen.getHeight() - Gdx.input.getY() / screen.getScaleHeight() + screen.getCamera().position.y - 
+            float y = screen.getHeight() - Gdx.input.getY() / screen.getScaleHeight() + screen.getCamera().position.y - 
                 screen.getCamera().viewportHeight/2;
 
-        for (ClickableComponent clickable : clickableComponents) {
-            if (clickable.isEnabled()) {
-                if (x > clickable.getPosition().x - clickable.getWidth() / 2 &&
-                        x < clickable.getPosition().x + clickable.getWidth() / 2 &&
-                        y > clickable.getPosition().y - clickable.getHeight() / 2 &&
-                        y < clickable.getPosition().y + clickable.getHeight() / 2) {
-                    clickable.trigger();
+            for (ClickableComponent clickable : clickableComponents) {
+                if (clickable.isEnabled()) {
+                    if (x > clickable.getPosition().x - clickable.getWidth() / 2 &&
+                            x < clickable.getPosition().x + clickable.getWidth() / 2 &&
+                            y > clickable.getPosition().y - clickable.getHeight() / 2 &&
+                            y < clickable.getPosition().y + clickable.getHeight() / 2) {
+                        clickable.trigger();
+                    }
                 }
             }
         }
     }
-
-    public void add(ClickableComponent component) {
-        if (!clickableComponents.contains(component)) {
-            clickableComponents.add(component);
-        }
-    }
-
-    public void remove(ClickableComponent component) {
-        if (clickableComponents.contains(component)) {
-            clickableComponents.remove(component);
-        }
-    }
-
+    
+    // Called when loading a new screen
     @Override
     protected void loadScreenSettings() {
-
+        DebugTool.log("InputManager_loadScreenSettings: Complete");
     }
-
+    
+    // Called when unloading the current screen
     @Override
     protected void removeScreenSettings() {
-
+        DebugTool.log("InputManager_removeScreenSettings: Complete");
     }
+    
+    // Called to add ClickableComponent to @clickableComponents
+    public void add(ClickableComponent clickable) {
+        if (!clickableComponents.contains(clickable)) {
+            clickableComponents.add(clickable);
+        } else {
+            ErrorTool.log("Failed to add clickable to clickableComponents");  
+        }
+    }
+
+    // Called to remove ClickableComponent from @clickableComponents
+    public void remove(ClickableComponent clickable) {
+        if (clickableComponents.contains(clickable)) {
+            clickableComponents.remove(clickable);
+        } else {
+            ErrorTool.log("Failed to remove clickable to clickableComponents");  
+        }
+    }
+
 }
