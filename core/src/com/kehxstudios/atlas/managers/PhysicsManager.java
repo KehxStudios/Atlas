ab9fccaaa3694fd9e893cbbee55ff9ae2e132247
Christopher Reid <Christopher.Reid@KehxStudios.Com>
2017-05-10 10:02:10 -0300

    Edited PhysicsManager on GitHub site
    - Organized and cleaned up some of the code
    - Added basic comments to all methods
    - Changed some Debug logs to Error logs
    - Added some Debug logs to ensure proper setup

--- core/src/com/kehxstudios/atlas/managers/PhysicsManager.java
+++ core/src/com/kehxstudios/atlas/managers/PhysicsManager.java
@@ -2,6 +2,7 @@ package com.kehxstudios.atlas.managers;
 
 import com.kehxstudios.atlas.components.PhysicsComponent;
 import com.kehxstudios.atlas.tools.DebugTool;
+import com.kehxstudios.atlas.tools.ErrorTool;
 
 import java.util.ArrayList;
 
@@ -11,6 +12,7 @@ import java.util.ArrayList;
 
 public class PhysicsManager extends Manager {
 
+    // Holds instance of class, create new if not set
     private static PhysicsManager instance;
     public static PhysicsManager getInstance() {
         if (instance == null) {
@@ -19,9 +21,26 @@ public class PhysicsManager extends Manager {
         return instance;
     }
 
+    // ArrayList for all PhysicsComponents created
     private ArrayList<PhysicsComponent> physicsComponents;
+    // PhysicsComponent that collision will be checked against, will be removed for complete collision later
     private PhysicsComponent player;
 
+    // Constructor
+    private PhysicsManager() {
+        super();
+        setup();
+    }
+    
+    // Initalize @physicsComponents
+    @Override
+    private void setup() {
+        physicsComponents = new ArrayList<PhysicsComponent>();
+        DebugTool.log("PhysicsManager_setup: Complete");
+    }
+    
+    // Called to update all PhysicsComponents and then check for collision
+    @Override
     public void tick(float delta) {
         for (PhysicsComponent physics : physicsComponents) {
             if (physics.isEnabled()) {
@@ -34,57 +53,51 @@ public class PhysicsManager extends Manager {
             }
         }
 
-        if (player == null) {
-            return;
-        }
-        for (PhysicsComponent physic : physicsComponents) {
-            if (physic.isEnabled() && physic.collidable && physic != player) {
-                if (player.getBounds().overlaps(physic.getBounds())) {
-                    DebugTool.log("Collision");
-                    player.setCollided(true);
+        if (player != null) {
+            for (PhysicsComponent physic : physicsComponents) {
+                if (physic.isEnabled() && physic.collidable && physic != player) {
+                    if (player.getBounds().overlaps(physic.getBounds())) {
+                        DebugTool.log("Collision");
+                        player.setCollided(true);
+                    }
                 }
             }
         }
-
     }
-
-    public void setPlayer(PhysicsComponent player) {
-        this.player = player;
-        DebugTool.log("Player set in PhysicsManager");
-    }
-
+    
+    // Called when loading a new screen
     @Override
     protected void loadScreenSettings() {
-
+         DebugTool.log("PhysicsManager_loadScreenSettings: Complete");
     }
 
+    // Called when unloading the current screen
     @Override
     protected void removeScreenSettings() {
-
+        DebugTool.log("PhysicsManager_removeScreenSettings: Complete");
+    }
+    
+    // Called to set the current @player for collision
+    public void setPlayer(PhysicsComponent player) {
+        this.player = player;
+        DebugTool.log("PhysicsManager_setPlayer: Complete");
     }
 
+    // Adds a physics to @physicsComponents
     public void add(PhysicsComponent physics) {
         if (!physicsComponents.contains(physics)) {
             physicsComponents.add(physics);
         } else {
-            DebugTool.log("Failed to add physics to physicsComponents");
+            ErrorTool.log("Failed to add physics to physicsComponents");
         }
     }
 
+    // Removes a physics from @physicsComponents
     public void remove(PhysicsComponent physics) {
         if (physicsComponents.contains(physics)) {
             physicsComponents.remove(physics);
         } else {
-            DebugTool.log("Failed to find physics in physicsComponents");
+            ErrorTool.log("Failed to find physics in physicsComponents");
         }
-    }
-
-    private PhysicsManager() {
-        super();
-        setup();
-    }
-    
-    private void setup() {
-        physicsComponents = new ArrayList<PhysicsComponent>();
     }
 }
