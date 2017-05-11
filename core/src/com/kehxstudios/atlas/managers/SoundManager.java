2a0790f74d87494a6e30e4b84811163e33976f63
Christopher Reid <Christopher.Reid@KehxStudios.Com>
2017-05-11 15:40:26 -0300

    Created SoundManager
    First version of SoundManager that will control the future SoundComponent & MusicComponent.  Will lately change based on how the files are loaded but the layout should remain the same.

---
+++ core/src/com/kehxstudios/atlas/managers/SoundManager.java
@@ -0,0 +1,109 @@
+package com.kehxstudios.atlas.managers;
+
+
+
+public class SoundManager extends Manager {
+
+    // Holds instance of class, create new if not set
+    private static SoundManager instance;
+    public static SoundManager getInstance() {
+        if (instance == null) {
+            instance = new SoundManager();
+        }
+        return instance;
+    }
+    
+    private ArrayList<SoundComponent> soundComponents;
+    private ArrayList<MusicComponent> musicComponents;
+
+    public SoundManager() {
+        super();
+        setup();
+    }
+
+
+    @Override
+    public void tick(float delta) {
+        
+    }
+    
+    @Override
+    protected void setup() {
+        soundComponents = new ArrayList<SoundComponent>();
+        musicComponents = new ArrayList<MusicComponent>();
+    }
+    
+    @Override
+    protected void loadScreenSettings() {
+        DebugTool.log("SoundManager_loadScreenSettings: Complete");
+    }
+    
+    @Override
+    protected void removeScreenSettings() {
+        DebugTool.log("SoundManager_loadScreenSettings: Complete");
+    }
+    
+    public void play(Component component) {
+        if (component.getType() == ComponentType.SOUND) {
+            SoundComponent sound = (SoundComponent)component;
+            if (!soundComponents.contains(sound) {
+                add(sound);
+            }
+            // music.getClip().play();
+        } else if (component.getType() == ComponentType.MUSIC) {
+            MusicComponent music = (MusicComponent)component;
+            if (!musicComponents.contains(music) {
+                add(music);
+            }
+            // music.getClip().play();
+        }
+    }
+    
+    public void stop(Component component) {
+        if (component.getType() == ComponentType.SOUND) {
+            SoundComponent sound = (SoundComponent)component;
+            if (!soundComponents.contains(sound) {
+                add(sound);
+            }
+            //if (sound.getClip().isPlaying()) {
+            //    sound.getClip().stop();
+            //}
+        } else if (component.getType() == ComponentType.MUSIC) {
+            MusicComponent music = (MusicComponent)component;
+            if (!musicComponents.contains(music) {
+                add(music);
+            }
+            //if (music.getClip().isPlaying()) {
+            //    music.getClip().stop();
+            //}
+        }
+    }
+    
+    public void add(Component component) {
+        if (component.getType() == ComponentType.SOUND) {
+            SoundComponent sound = (SoundComponent)component;
+            if (!soundComponents.contains(sound) {
+                soundComponents.add(sound);
+            }
+        } else if (component.getType() == ComponentType.MUSIC) {
+            MusicComponent music = (MusicComponent)component;
+            if (!musicComponents.contains(music) {
+                musicComponets.add(music);
+            }
+        }
+    }
+    
+    public void remove(Component component) {
+        if (component.getType() == ComponentType.SOUND) {
+            SoundComponent sound = (SoundComponent)component;
+            if (soundComponents.contains(sound) {
+                soundComponents.remove(sound);
+            }
+        } else if (component.getType() == ComponentType.MUSIC) {
+            MusicComponent music = (MusicComponent)component;
+            if (musicComponents.contains(music) {
+                musicComponets.remove(music);
+            }
+        }
+    }
+}
