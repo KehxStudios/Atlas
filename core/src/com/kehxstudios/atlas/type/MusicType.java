498fe93f2c2f42689c99e5de579ffa6cd7306ceb
Christopher Reid <Christopher.Reid@KehxStudios.Com>
2017-05-11 15:24:26 -0300

    Created MusicType
    MusicType will be used to store the name and location of a music file to be used when loading or may be streamed.

---
+++ core/src/com/kehxstudios/atlas/type/MusicType.java
@@ -0,0 +1,38 @@
+package com.kehxstudios.atlas.type;
+
+import java.util.HashMap;
+
+public enum MusicType {
+
+    INTRO("Intro", "intro.mp3", "music/intro/", 3.0f), 
+    
+    MAIN_MENU("MainMenu", "mainMenu.mp3", "music/mainMenu/", 5.0f),
+    
+    VOID("Void", "", "", 0.0f);
+
+    private String id, fileName, filePath;
+
+    private MusicType(String id, String fileName, String filePath) {
+        this.id = id;
+        this.fileName = fileName;
+        this.filePath = filePath;
+    }
+
+    public String getId() { return id; }
+
+    public String getFileName() { return fileName; }
+    
+    public String getFilePath() { return filePath; }
+    
+    public String getCompletePath() { return filePath + fileName; }
+    
+    public MusicType getTypeById(String id) { return musicTypes.get(id); }
+
+    private static HashMap<String, MusicType> musicTypes;
+
+    static {
+        musicTypes = new HashMap<String, MusicType>();
+        for (MusicType type : MusicType.values()) {
+            musicTypes.put(type.id, type);
+    }
+}
