0d01582dc01e0d03a2b9cd296fcf1d73800e9aab
Christopher Reid <Christopher.Reid@KehxStudios.Com>
2017-05-11 15:19:12 -0300

    Created SoundType
    SoundType will be used to store the name and location of a sound file to be used when loading.

---
+++ core/src/com/kehxstudios/atlas/type/SoundType.java
@@ -0,0 +1,42 @@
+package com.kehxstudios.atlas.type;
+
+import java.util.HashMap;
+
+public enum SoundType {
+
+    INTRO("Intro", "intro.mp3", "screens/intro/", 3.0f), 
+    
+    MAIN_MENU("MainMenu", "mainMenu.mp3", "screens/mainMenu/", 5.0f),
+    
+    VOID("Void", "", "", 0.0f);
+
+    private String id, fileName, filePath;
+    private float length;
+
+    private SoundType(String id, String fileName, String filePath, float length) {
+        this.id = id;
+        this.fileName = fileName;
+        this.filePath = filePath;
+        this.length = length;
+    }
+
+    public String getId() { return id; }
+
+    public String getFileName() { return fileName; }
+    
+    public String getFilePath() { return filePath; }
+    
+    public String getCompletePath() { return filePath + fileName; }
+    
+    public float getLength() { return length; }
+
+    public SoundType getTypeById(String id) { return soundTypes.get(id); }
+
+    private static HashMap<String, SoundType> soundTypes;
+
+    static {
+        soundTypes = new HashMap<String, SoundType>();
+        for (SoundType type : SoundType.values()) {
+            soundTypes.put(type.id, type);
+    }
+}
