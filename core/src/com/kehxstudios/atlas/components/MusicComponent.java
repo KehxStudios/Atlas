91b95ea22858a0376573655ea45ab5d789978b0a
Christopher Reid <Christopher.Reid@KehxStudios.Com>
2017-05-12 09:28:32 -0300

    Edited MusicComponent
    - Added use of LibGDX's Music class for audio
    - Added use of MusicType for shared variables
    - Edited remaining variables.

--- core/src/com/kehxstudios/atlas/components/MusicComponent.java
+++ core/src/com/kehxstudios/atlas/components/MusicComponent.java
@@ -3,7 +3,37 @@ package com.kehxstudios.atlas.components;
 
 public class MusicComponent extends Component {
 
-    private float currentTime;
-    private boolean looped, finished;
+    private MusicType musicType
+    private Music music;
+    private float volume;
     
+    public MusicComponent() {
+        super();   
+    }
+    
+    public void setMusicType(MusicType musicType) {
+        this.musicType = musicType;
+        if (musicType == MusicType.VOID) {
+            setEnabled(false);
+        } else {
+            music = SoundManager.getInstance().getMusic(soundType);
+        }
+    }
+    public MusicType getMusicType() { return musicType; }
+    
+    public Music getMusic() { return music; }
+    
+    public void setVolume(float volume) { 
+        this.volume = volume; 
+        if (volume > 1f) {
+            volume = 1f;   
+        } else if (volume < 0f) {
+            volume = 0f;   
+        }
+        music.setVolume(volume);
+    }
+    public void changeVolume(float value) {
+        setVolume(volume + value);
+    }
+    public float getVolume() { return volume; }
 }
