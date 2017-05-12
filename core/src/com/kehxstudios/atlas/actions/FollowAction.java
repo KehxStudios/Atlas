8097a852abfd76ec827d16678d75a7bcb948970c
Christopher Reid <Christopher.Reid@KehxStudios.Com>
2017-05-12 10:08:53 -0300

    Created FollowAction
    This will be used with a ClickableComponent to move the Entity/Component based on what is passed by getting the position clicked from InputManager

---
+++ core/src/com/kehxstudios/atlas/actions/FollowAction.java
@@ -0,0 +1,28 @@
+package com.kehxstudios.atlas.actions;
+
+import com.badlogic.gdx.math.Vector2;
+
+public class FollowAction extends Action {
+ 
+    private Vector2 position
+    private boolean verticalAllowed, horizontalAllowed;
+    
+    public FollowAction() {
+        actionType = ActionType.FOLLOW;
+    }
+    
+    public void setPosition(Vector2 position) { this.position = position; }
+
+    public void setVerticalAllowed(boolean value) { verticalAllowed = value; }
+    public void setHorizontalAllowed(boolean value) { horizontalAllowed = value; }
+
+    public void trigger() {
+        Vector2 clicked = InputManager.getInstance().getClickedPosition();
+        if (verticalAllowed && horizontalAllowed) {
+            position.set(clicked.x, clicked.y);
+        } else if (verticalAllowed) {
+            position.x = clicked.x;
+        } else if (horizontalAllowed) {
+            position.y = clicked.y;
+        }
+    }
