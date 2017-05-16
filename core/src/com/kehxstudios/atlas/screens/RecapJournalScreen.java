0adbd3f6275d58978447e95724003d253e880025
Christopher Reid <Christopher.Reid@KehxStudios.Com>
2017-05-16 12:47:29 -0300

    Updated RecapJournalScreen
    - Added copyright information & class description comments

--- core/src/com/kehxstudios/atlas/screens/RecapJournalScreen.java
+++ core/src/com/kehxstudios/atlas/screens/RecapJournalScreen.java
@@ -1,3 +1,22 @@
+/*******************************************************************************
+ * Copyright 2017 See AUTHORS file.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
+ * associated documentation files (the "Software"), to deal in the Software without restriction, 
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, 
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial 
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT 
+ * LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ ******************************************************************************/
+
 package com.kehxstudios.atlas.screens;
 
 import com.kehxstudios.atlas.entities.Entity;
@@ -9,15 +28,16 @@ import com.kehxstudios.atlas.tools.Templates;
 import com.kehxstudios.atlas.type.ScreenType;
 import com.kehxstudios.atlas.type.TextureType;
 
+/**
+ * A text journal for daily tracking, to be functional in the near future
+ */
 
 public class RecapJournalScreen extends AScreen {
-
 
     public RecapJournalScreen() {
         super(ScreenType.RECAP_JOURNAL);
         
     }
-
 
     public void finalizeSetup() {
         super.finalizeSetup();
