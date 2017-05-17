5ef73619a34e7d0e1afcc8ce2b7044b0bea4bfe9
Christopher Reid <Christopher.Reid@KehxStudios.Com>
2017-05-17 09:59:24 -0300

    AnimationComponent ECS Update
    - Made all variables public with no getters/setters
    - Removed all functions
    - Removed unnecessary variables

--- core/src/com/kehxstudios/atlas/components/AnimationComponent.java
+++ core/src/com/kehxstudios/atlas/components/AnimationComponent.java
@@ -27,18 +27,9 @@ import com.kehxstudios.atlas.type.TextureType;
 
 public class AnimationComponent extends Component {
 
-    private GraphicsComponent graphicsComponent;
-    private TextureType[] graphics;
-    private int graphicIndex;
-    private float timePerGraphic;
-    private float currentTime;
-    private boolean looped, finished;
-
-    public void setLooped(boolean value) { looped = value; }
-
-    public void setTimePerGraphic(float value) { timePerGraphic = value; }
-
-    public void scaleTimePerGraphic(float value) { timePerGraphic *= value; }
-
-    public boolean isFinished() { return finished; }
+    public TextureType[] graphics;
+    public int graphicIndex;
+    public float timePerGraphic;
+    public float currentTime;
+    public boolean looped, finished;
 }
