306a902c5c1f5065ee3653dc9ac7df04bebf22b8
Christopher Reid <Christopher.Reid@KehxStudios.Com>
2017-05-17 09:55:06 -0300

    MusicComponent ECS Update
    - Made all variables public with no getters/setters
    - Removed all functions
    - Removed unnecessary variables

--- core/src/com/kehxstudios/atlas/components/MusicComponent.java
+++ core/src/com/kehxstudios/atlas/components/MusicComponent.java
@@ -21,7 +21,6 @@ package com.kehxstudios.atlas.components;
 
 
 import com.badlogic.gdx.audio.Music;
-import com.kehxstudios.atlas.managers.SoundManager;
 import com.kehxstudios.atlas.type.MusicType;
 
 /**
@@ -30,37 +29,7 @@ import com.kehxstudios.atlas.type.MusicType;
 
 public class MusicComponent extends Component {
 
-    private MusicType musicType;
-    private Music music;
-    private float volume;
-    
-    public MusicComponent() {
-        super();   
-    }
-    
-    public void setMusicType(MusicType musicType) {
-        this.musicType = musicType;
-        if (musicType == MusicType.VOID) {
-            setEnabled(false);
-        } else {
-            music = SoundManager.getInstance().getMusic(musicType);
-        }
-    }
-    public MusicType getMusicType() { return musicType; }
-    
-    public Music getMusic() { return music; }
-    
-    public void setVolume(float volume) { 
-        this.volume = volume; 
-        if (volume > 1f) {
-            volume = 1f;   
-        } else if (volume < 0f) {
-            volume = 0f;   
-        }
-        music.setVolume(volume);
-    }
-    public void changeVolume(float value) {
-        setVolume(volume + value);
-    }
-    public float getVolume() { return volume; }
+    public MusicType musicType;
+    public Music music;
+    public float volume;
 }
