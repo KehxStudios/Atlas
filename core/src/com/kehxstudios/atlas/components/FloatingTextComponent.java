bc69e10b56c688ed5f3732adc167dea3ed019341
Christopher Reid <Christopher.Reid@KehxStudios.Com>
2017-05-17 09:46:42 -0300

    FloatingTextComponent ECS Update
    - Made all variables public with no getters/setters
    - Removed all functions
    - Removed unnecessary variables

--- core/src/com/kehxstudios/atlas/components/FloatingTextComponent.java
+++ core/src/com/kehxstudios/atlas/components/FloatingTextComponent.java
@@ -31,37 +31,10 @@ import com.kehxstudios.atlas.entities.Entity;
 
 public class FloatingTextComponent extends Component{
 
-    private BitmapFont font;
-    private GlyphLayout glyphLayout;
-    private String label;
-    private String text;
-    private Color color;
-    private boolean isUIElement;
-
-    public BitmapFont getFont() { return font; }
-    public void setFont(BitmapFont font) { this.font = font;}
-
-    public GlyphLayout getLayout() {
-        return glyphLayout;
-    }
-    public void setLayout(GlyphLayout glyphLayout) { this.glyphLayout = glyphLayout; }
-
-    public String getLabel() { return label; }
-    public void setLabel(String label) { this.label = label; }
-
-    public String getText() { return text; }
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public Color getColor() { return color; }
-    public void setColor(Color color) { this.color = color; }
-
-    public void setScale(float scale) {
-        font.getData().setScale(scale);
-    }
-
-    public boolean getIsUIElement() { return isUIElement; }
-
-    public void setUIElement(boolean value) { isUIElement = value; }
+    public BitmapFont font;
+    public GlyphLayout glyphLayout;
+    public String label;
+    public String text;
+    public float scale;
+    public Color color;
 }
