34f14dec89fda3c7e27993242598ad6eef59056e
Christopher Reid <Christopher.Reid@KehxStudios.Com>
2017-05-17 09:49:05 -0300

    GraphicsComponent ECS Update
    - Made all variables public with no getters/setters
    - Removed all functions
    - Removed unnecessary variables

--- core/src/com/kehxstudios/atlas/components/GraphicsComponent.java
+++ core/src/com/kehxstudios/atlas/components/GraphicsComponent.java
@@ -20,7 +20,6 @@
 package com.kehxstudios.atlas.components;
 
 import com.badlogic.gdx.graphics.Texture;
-import com.kehxstudios.atlas.managers.GraphicsManager;
 import com.kehxstudios.atlas.type.TextureType;
 
 /**
@@ -29,44 +28,9 @@ import com.kehxstudios.atlas.type.TextureType;
 
 public class GraphicsComponent extends Component {
 
-    private TextureType textureType;
-    private Texture texture;
-    private float width, height;
-    private int layer;
-    private float rotation;
-
-    public TextureType getTextureType() { return textureType; }
-    public void setTextureType(TextureType textureType) {
-        this.textureType = textureType;
-        width = textureType.getWidth();
-        height = textureType.getHeight();
-        if (textureType != TextureType.VOID) {
-            texture = GraphicsManager.getInstance().getTexture(textureType);
-        }
-    }
-    
-    public Texture getTexture() { return texture; }
-
-    public float getWidth() { return width; }
-    public void setWidth(float width) { this.width = width; }
-
-    public float getHeight() {
-        return height;
-    }
-    public void setHeight(float height) { this.height = height; }
-
-    public int getLayer() { return layer; }
-    public void setLayer(int layer) { this.layer = layer; }
-    
-    public float getRotation() { return rotation; }
-    public void addRotation(float value) { setRotation(rotation + value); }
-    public void setRotation(float rotation) { 
-        while (rotation > 360f) {
-            rotation -= 360f;
-        }
-        while (rotation < 0f) {
-            rotation += 360f;   
-        }
-        this.rotation = rotation; 
-    }
+    public TextureType textureType;
+    public Texture texture;
+    public float width, height;
+    public int layer;
+    public float rotation;
 }
