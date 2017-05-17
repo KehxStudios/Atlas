5910e9c6850d94adf38d5cc4371a08394c3bcc4a
Christopher Reid <Christopher.Reid@KehxStudios.Com>
2017-05-17 10:37:04 -0300

    ComponentData ECS Update
    - Removed unnecessary variables & methods

--- core/src/com/kehxstudios/atlas/data/ComponentData.java
+++ core/src/com/kehxstudios/atlas/data/ComponentData.java
@@ -27,59 +27,21 @@ import java.util.HashMap;
 
 public class ComponentData {
 
-    public String id;
     public String type;
-    public String entityId;
     public boolean enabled;
-    public float x, y;
-    public boolean useComponentPosition;
-    public boolean usePositionAsOffset;
     public HashMap<String, String> data;
 
     public ComponentData() {
-        x = 0;
-        y = 0;
-        usePositionAsOffset = false;
-        useComponentPosition = false;
+        type = ComponentType.VOID.getId();
         enabled = true;
         data = new HashMap<String, String>();
     }
 
-    public String getId() { return id; }
-
-    public void setId(String id) { this.id = id; }
-
-    public boolean getUseComponentPosition() { return useComponentPosition; }
-
-    public void setUseComponentPosition(boolean value) { useComponentPosition = value; }
-
-    public boolean getUsePositionAsOffset() { return usePositionAsOffset; }
-
-    public void setUsePositionAsOffset(boolean value) { usePositionAsOffset = value; }
-
-    public String getType() {
-        return type;
+    public ComponentData(ComponentType componentType, boolean enabled) {
+        type = componentType.getId();
+        this.enabled = enabled;
+        data = new HashMap<String, String>();
     }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getEntityId() { return entityId; }
-
-    public void setEntityId(String entityId) { this.entityId = entityId; }
-
-    public boolean isEnabled() { return enabled; }
-
-    public void setEnabled(boolean value) { enabled = value; }
-
-    public float getX() { return x; }
-
-    public void setX(float x) { this.x = x; }
-
-    public float getY() { return y; }
-
-    public void setY(float y) { this.y = y; }
 
     public void putFloat(String key, float value) {
         data.put(key, "" + value);
